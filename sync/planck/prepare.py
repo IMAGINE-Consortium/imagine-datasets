684f9fbc94f1fc105512d1c698ebebba3a247ffc
Theo Steininger <theo.steininger@ultimanet.de>
2017-12-12 03:34:34 +0100

    Refactoring and reordering.

--- sync/planck/prepare.py
+++ sync/planck/prepare.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7365bf3ec6cb882bad3a46632d385c3f8f642baa432f91d3462b8fc3930e683d
-size 1772
+oid sha256:989bf86b3a7c07c1bb1f208718f1389188f726b1743ebc04df54573c8c3c63c6
+size 1781
