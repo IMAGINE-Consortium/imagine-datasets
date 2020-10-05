e6d2f21c2b7feccc8114a9fb86aa1b631afd2c9e
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2020-10-05 15:19:14 +0200

    Starts reorganising repository

---
+++ imagine_datasets/__version__.py
@@ -0,0 +1,8 @@
+"""
+IMAGINE-datasets Version
+=====================
+Stores the different versions of the *IMAGINE-datasets* package.
+"""
+# %% VERSIONS
+# Default/Latest/Current version
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,35 @@
+# Built-in imports
+from codecs import open
+import re
+
+# Package imports
+from setuptools import find_packages, setup
+
+# Get the requirements list
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+# Read the __version__.py file
+with open('imagine_datasets/__version__.py', 'r') as f:
+    vf = f.read()
+
+# Obtain version from read-in __version__.py file
+version = re.search(r"^_*version_* = ['\"]([^'\"]*)['\"]", vf, re.M).group(1)
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name="imagine-datasets",
+      version=version,
+      description="Dataset repository",
+      license="GPLv3",
+      url="https://github.com/IMAGINE-Consortium/imagine-dataset",
+      author="Luiz Felippe S. Rodrigues, IMAGINE Consortium",
+      author_email="luizfelippesr@alumni.usp.br",
+      maintainer="IMAGINE Consortium",
+      packages=find_packages(),
+      include_package_data=True,
+      platforms=['Linux', 'Unix'],
+      python_requires='>=3.5',
+      install_requires=requirements)
+
